6cd0cc405fad12ca83cf7b6858537ffa6f3f6041
Cristian Berceanu <cristi92b@gmail.com>
2018-10-14 21:17:47 +0300

    Trying to capute packet

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,51 @@
 #include <iostream>
 #include <pcap.h>
 #include "Packet.h"
+#include <time.h>
+#include <netinet/in.h>
+#include <netinet/if_ether.h>
+
+void print_packet_info(const u_char *packet, struct pcap_pkthdr packet_header);
 
 int main(int argc, char **argv) {
-    char *device; /* Name of device (e.g. eth0, wlan0) */
-    char error_buffer[PCAP_ERRBUF_SIZE]; /* Size defined in pcap.h */
+    char *device;
+    char error_buffer[PCAP_ERRBUF_SIZE];
+    pcap_t *handle;
+    const u_char *packet;
+     struct pcap_pkthdr packet_header;
+    int packet_count_limit = 1;
+    int timeout_limit = 10000; /* In milliseconds */
 
-    /* Find a device */
     device = pcap_lookupdev(error_buffer);
     if (device == NULL) {
-        std::cout << "Error finding device: " << error_buffer;
+        printf("Error finding device: %s\n", error_buffer);
         return 1;
     }
-    std::cout << "Network device found: " << device;
+
+    /* Open device for live capture */
+    handle = pcap_open_live(
+            device,
+            BUFSIZ,
+            packet_count_limit,
+            timeout_limit,
+            error_buffer
+        );
+
+     /* Attempt to capture one packet. If there is no network traffic
+      and the timeout is reached, it will return NULL */
+     packet = pcap_next(handle, &packet_header);
+     if (packet == NULL) {
+        printf("No packet found.\n");
+        return 2;
+    }
+
+    /* Our function to output some info */
+    print_packet_info(packet, packet_header);
+
     return 0;
+}
+
+void print_packet_info(const u_char *packet, struct pcap_pkthdr packet_header) {
+    printf("Packet capture length: %d\n", packet_header.caplen);
+    printf("Packet total length %d\n", packet_header.len);
 }
