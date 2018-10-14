90b809cdc8f6fc343bb0b7daa9cc29f83820b5a5
Cristian Berceanu <cristi92b@gmail.com>
2018-10-15 00:12:32 +0300

    working example

--- Main.cpp
+++ Main.cpp
@@ -4,17 +4,64 @@
 #include <time.h>
 #include <netinet/in.h>
 #include <netinet/if_ether.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
-void print_packet_info(const u_char *packet, struct pcap_pkthdr packet_header);
+//void print_packet_info(const u_char *packet, struct pcap_pkthdr packet_header);
 
 int main(int argc, char **argv) {
+
+    char *device; /* Name of device (e.g. eth0, wlan0) */
+    char error_buffer[PCAP_ERRBUF_SIZE]; /* Size defined in pcap.h */
+
+    /* Find a device */
+    device = pcap_lookupdev(error_buffer);
+    if (device == NULL) {
+        printf("Error finding device: %s\n", error_buffer);
+        return 1;
+    }
+    printf("Network device found: %s\n", device);
+
+    //char ip[13];
+    std::string ipAddr;
+    char subnet_mask[13];
+    bpf_u_int32 ip_raw; /* IP address as integer */
+    bpf_u_int32 subnet_mask_raw; /* Subnet mask as integer */
+    int lookup_return_code;
+    struct in_addr address; /* Used for both ip & subnet */
+
+    lookup_return_code = pcap_lookupnet(
+        device,
+        &ip_raw,
+        &subnet_mask_raw,
+        error_buffer
+    );
+    if (lookup_return_code == -1) {
+        printf("%s\n", error_buffer);
+        return 1;
+    }
+
+    address.s_addr = ip_raw;
+    ipAddr = inet_ntoa(address);
+    if (ipAddr.empty()) {
+        printf("inet_ntoa"); /* print error */
+        return 1;
+    }
+    else
+    {
+        std::cout << "IP=" << ipAddr.c_str();
+    }
+
+    return 0;
+
+/*
     char *device;
     char error_buffer[PCAP_ERRBUF_SIZE];
     pcap_t *handle;
     const u_char *packet;
      struct pcap_pkthdr packet_header;
     int packet_count_limit = 1;
-    int timeout_limit = 10000; /* In milliseconds */
+    int timeout_limit = 10000;
 
     device = pcap_lookupdev(error_buffer);
     if (device == NULL) {
@@ -22,7 +69,7 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    /* Open device for live capture */
+
     handle = pcap_open_live(
             device,
             BUFSIZ,
@@ -31,20 +78,17 @@ int main(int argc, char **argv) {
             error_buffer
         );
 
-     /* Attempt to capture one packet. If there is no network traffic
-      and the timeout is reached, it will return NULL */
      packet = pcap_next(handle, &packet_header);
      if (packet == NULL) {
         std::cout << "No packet found" << std::endl;
         return 2;
     }
 
-    /* Our function to output some info */
-    print_packet_info(packet, packet_header);
+*/
+    //print_packet_info(packet, packet_header);
 
-    return 0;
 }
-
+/*
 void print_packet_info(const u_char *packet, struct pcap_pkthdr packet_header) {
     if(packet != NULL)
     {
@@ -52,3 +96,4 @@ void print_packet_info(const u_char *packet, struct pcap_pkthdr packet_header) {
         std::cout << "Packet total length:" <<  packet_header.len << std::endl;
     }
 }
+*/
